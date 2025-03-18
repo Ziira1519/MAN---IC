0e4e6781408f571bc1dbf16772a4f3e2b249294c
ziira1519 <zahiraputri.zpf@gmail.com>
2025-03-18 14:24:28 +0700

    [FEAT]firs commit

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+// console.log("halo,bumii");
+// document.write("saya senanggg")
+
+let nama;
+nama = "zahira putri";
+console.log(nama);
+nama = "danisha"
+console.log("nama")
+
+var nama1
+// const nama2
